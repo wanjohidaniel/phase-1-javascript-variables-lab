0a2cdf703827d7f9f24447689a23cbcbb038c164
Daniel Wanjohi <danirunguwanjohi@gmail.com>
2024-03-19 06:36:54 -0700

    Variable labs

--- index.js
+++ index.js
@@ -1,1 +1,9 @@
 // Code your solution in this file!
+let companyName = 'Scuber';
+/const companyName/
+
+let mostProfitableNeighborhood = 'Chelsea';
+/mostProfitableNeighborhood/
+
+let companyCeo = 'Susan Smith';
+/let companyCeo/
